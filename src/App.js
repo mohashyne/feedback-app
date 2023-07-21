222cc2925a9a30963b92ab9a5cb31bdc5d91de1c
MSA <crystalbluetech@gmail.com>
2023-07-21 13:45:27 +0100

    Added file and structures

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="App">
+        <header className="App-header">
+            <h1>Feedback App</h1>
+        </header>
+        <main>
+            <p >Rate your experience</p>
+        </main>
+        </div>
+    );  
+}
+
+
+export default App;
